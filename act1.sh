f939cee0ab5e3c3eb3abcc2bf0cbca66fe16ff07
Tony K <anthonymkelly93@gmail.com>
2022-03-23 12:30:37 -0400

    Complete

--- act1.sh
+++ act1.sh
@@ -1,13 +1,17 @@
 # 1. In this directory, create a new directory called star_wars. Example answer: mkdir star_wars
 mkdir star_wars
 # 2. In the star_wars folder, create two new directories: empire and rebellion (This can be done in two commands, but how would you do it in one?)
-
+mkdir empire
+mkdir rebellion
 # 3. Inside the empire directory, create a file called darth_vader.txt 
-
+cd empire
+ New-Item ~\SEI-321\Star_Wars\empire\darth_vader.txt
 # 4. Use the force (or your echo) to add the text "...heavy breathing..." to the darth_vader.txt file (Don’t remember how to do this? Use the ‘other force’, known as Google!)
-
+Got confused and typed direcly into file, but now know:
+echo "...heavy breathing..."
 # 5. Inside the empire directory, create a file called emperor_palpatine.txt
-
+New-Item ~\SEI-321\Star_Wars\empire\emperor_palpatine.txt
 # 6. Inside the empire directory, create a directory called death_star
-
+mkdir ~\SEI-321\Star_Wars\empire\death_star
 # 7. Move darth_vader.txt into the death_star
+mv ~\SEI-321\Star_Wars\empire\darth_vader.txt ~\SEI-321\Star_Wars\empire\death_star
--- act2.sh
+++ act2.sh
@@ -1,13 +1,19 @@
 # 1. Inside the `star_wars/rebellion` directory, (IN ONE COMMAND) create a file called princess_leia.txt with the text "Help me, Obi-Wan…You’re my only hope."
-
+cd ~\SEI-321\Star_Wars\rebellion
+New-Item ~\SEI-321\Star_Wars\rebellion\princess_leia.text
+(Typed the text  directly into the file again...)
 # 2. Create a file called obi_wan.txt in star_wars/rebellion
-
+New-Item ~\SEI-321\Star_Wars\rebellion\obi_wan.txt
 # 3. Create a file in star_wars/rebellion called luke_skywalker.txt
-
+New-Item ~\SEI-321\Star_Wars\rebellion\luke_skywalker.txt
 # 4. Create a directory in star_wars/rebellion called millenium_falcon
-
+mkdir millenium_falcon
 # 5. Inside the millenium_falcon, create two files: han_solo.txt and chewbacca.txt
-
+New-Item han_solo.txt
+New-Item chewbacca.txt
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
-
+mv luke_skywalker.txt ~\SEI-321\Star_Wars\rebellion\millenium_falcon
+mv obi_wan.txt ~\SEI-321\Star_Wars\rebellion\millenium_falcon
+mv princess_leia.txt ~\SEI-321\Star_Wars\rebellion\millenium_falcon
 # 7. Move the millenium_falcon into the death_star.
+mv millenium_falcon ~\SEI-321\Star_Wars\empire\death_star
